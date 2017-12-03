956174aa6995addd0ca9c49d988f438e8cacf2b0
MVP <mrudulpen@hotmail.com>
2017-12-03 16:42:10 +0200

    Cucumber Feature File & CucumberTestBase

--- src/test/java/com/sits/amz/TestProductSearch.java
+++ src/test/java/com/sits/amz/TestProductSearch.java
@@ -1,12 +1,6 @@
 package com.sits.amz;
 
-import com.sits.amz.pages.HomePage;
-import com.sits.amz.pages.ProductDetailsPage;
-import com.sits.amz.pages.ProductListPage;
-import org.openqa.selenium.support.PageFactory;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
+/*
 public class TestProductSearch extends TestNgTestBase {
 
     private final static String productKeyWord = "Nikon";
@@ -27,4 +21,22 @@ public class TestProductSearch extends TestNgTestBase {
       String productTitle = productDetailsPage.getProductTitle();
       Assert.assertTrue(productTitle.contains(expectedProductTitle));
   }
+}
+*/
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Mrudul Pendharkar on 7/3/2017.
+ */
+
+public class TestProductSearch extends TestNgCucumberTestBase {
+
+    private final static String productKeyWord = "Nikon";
+    private final static String expectedProductTitle = "Nikon D3X";
+
+    @Test
+    public void testProductSearchByKeyWord() {
+
+    }
 }
